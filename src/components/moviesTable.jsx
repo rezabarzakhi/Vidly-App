e03b773dc9e8c6a5b258b38fa49a9a82e56e8f56
rezabarzakhi <reza.brzkhi@gmail.com>
2022-11-04 00:02:04 -0700

    create tableHeader Component
    create reusable TableHeader Class Component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,31 +1,25 @@
 import React, { Component } from "react";
 import Like from "./common/like";
+import TableHeader from "./common/tableHeader";
 
 class MoviesTable extends Component {
-  raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path) sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn)
-  };
-
+  colunms = [
+    { path: "title", lable: "Title" },
+    { path: "genre.name", lable: "Genre" },
+    { path: "numberInStock", lable: "Stock" },
+    { path: "dailyRentalRate", lable: "Rate" },
+    { key: "like" },
+    { key: "delete" },
+  ];
   render() {
-    const { movies, onDelete, onLike } = this.props;
+    const { movies, onDelete, onLike, sortColumn, onSort } = this.props;
     return (
       <table className="table w-75 h-auto ">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort("title")}>Title</th>
-            <th onClick={() => this.raiseSort("genre.name")}>Ganre</th>
-            <th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
-            <th onClick={() => this.raiseSort("dailyRentalRate")}>Rate</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
+        <TableHeader
+          columns={this.colunms}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
